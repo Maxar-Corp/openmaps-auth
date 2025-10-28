49e651b2e7f1fc46d140c1d02f970ea26d93baf6
omarfq-maxar <omar.flores@maxar.com>
2025-10-28 14:35:12 -0400

    lint

--- openmaps_auth/settings/dev.py
+++ openmaps_auth/settings/dev.py
@@ -8,36 +8,32 @@ OPENMAPS_AUTH_APP_LINKS = [
     {
         "link": "/",
         "text": "MapEdit",
-        "description": "Web-based mapping application that allows users to contribute and maintain vector data all over the world using high-resolution imagery."
+        "description": "Web-based mapping application that allows users to contribute and maintain vector data all over the world using high-resolution imagery.",
     },
     {
         "link": "/josm/",
         "text": "JOSM",
-        "description": "Java OpenStreetMap Editor is a desktop application for editing OpenStreetMap."
+        "description": "Java OpenStreetMap Editor is a desktop application for editing OpenStreetMap.",
     },
     {
         "link": "/geoserver/",
         "text": "GeoServer",
-        "description": "GeoServer is an open source server for sharing geospatial data."
+        "description": "GeoServer is an open source server for sharing geospatial data.",
     },
     {
         "link": "/hootenanny/",
         "text": "Hootenanny",
-        "description": "Hootenanny is a web-based application for editing OpenStreetMap data."
+        "description": "Hootenanny is a web-based application for editing OpenStreetMap data.",
     },
     {
         "link": "/certs/",
         "text": "Certificates",
-        "description": "Manage your client certificates."
+        "description": "Manage your client certificates.",
     },
-    {
-        "link": "/exports/",
-        "text": "Export Tool",
-        "description": "Tools for exporting."
-    },
+    {"link": "/exports/", "text": "Export Tool", "description": "Tools for exporting."},
     {
         "link": "/overpass/",
         "text": "Overpass Turbo",
-        "description": "Overpass Turbo is a web-based application for querying OpenStreetMap data."
+        "description": "Overpass Turbo is a web-based application for querying OpenStreetMap data.",
     },
 ]
